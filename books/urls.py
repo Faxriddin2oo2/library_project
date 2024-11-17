9fb6c79d866ff5a3415e5766c0f738de3f80b6e4
Faxriddin2oo2 <teshaboyevfaxriddin1@gmail.com>
2024-11-17 11:31:28 +0500

    Create CRUD

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,13 @@
+from django.template.context_processors import request
 from django.urls import path
 
-
-from .views import BookListApiView
+from .serializers import BookSerializer
+from .views import BookListApiView, book_list_view, BookDetailApiView, BookUpdateApiView, BookDeleteApiView
 
 urlpatterns = [
-    path('', BookListApiView.as_view(),)
+    path('', BookListApiView.as_view(),),
+    path('<int:pk>/', BookDetailApiView.as_view()),
+    path('<int:pk>/update', BookUpdateApiView.as_view()),
+    path('<int:pk>/delete', BookDeleteApiView.as_view()),
+    path('books/', book_list_view,)
 ]
--- books/views.py
+++ books/views.py
@@ -1,3 +1,6 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from .models import Book
 from .serializers import BookSerializer
 
@@ -7,3 +10,26 @@ from rest_framework import generics
 class BookListApiView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+class BookDetailApiView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookDeleteApiView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookUpdateApiView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+# Function based view in DRF
+@api_view(['GET'])
+def book_list_view(request, *args, **kwargs):
+    books = Book.objects.all()
+    serializer = BookSerializer(books, many= True)
+    return Response(serializer.data)
