4082f20a9499e041d7d3c16c7c9c28c7b2a40d40
Faxriddin2oo2 <teshaboyevfaxriddin1@gmail.com>
2024-11-19 19:23:01 +0500

    ModelSerializer vs Serializer

--- books/serializers.py
+++ books/serializers.py
@@ -7,3 +7,14 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ('id', 'title', 'content', 'subtitle', 'author', 'isbn', 'price',)
+
+
+
+# class BookSerializer(serializers.Serializer):
+#     title = serializers.CharField(max_length=200)
+#     content = serializers.CharField()
+#     subtitle = serializers.CharField()
+
+# class CashSerializer(serializers.Serializer):
+#     input = serializers.CharField(max_length=150)
+#     output = serializers.CharField(max_length=120)
