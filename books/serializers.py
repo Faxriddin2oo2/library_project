0c3754c09d93f2c54baa851bdc8cf9bde895957b
Faxriddin2oo2 <teshaboyevfaxriddin1@gmail.com>
2024-11-20 18:49:59 +0500

    Validation vs FieldValidation

--- books/serializers.py
+++ books/serializers.py
@@ -1,4 +1,7 @@
+from drf_yasg.codecs import VALIDATORS
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
 from .models import Book
 
 
@@ -8,7 +11,40 @@ class BookSerializer(serializers.ModelSerializer):
         model = Book
         fields = ('id', 'title', 'content', 'subtitle', 'author', 'isbn', 'price',)
 
+    def validate(self, data):
+        # print(data)
+        title = data.get('title', None)
+        author = data.get("author", None)
 
+
+        # check title if it contain only alphabetical chars
+        if title.isalpha():
+            raise ValidationError(
+                {
+                    "status" : False,
+                    "message" : "Kitobni sarlavhasi harflardan tashkil topgan bo'lishi kerak"
+                }
+            )
+
+        # check title and author from database existence
+        if Book.objects.filter(title=title, author=author).exists():
+            raise ValidationError(
+                {
+                    "status": False,
+                    "message": "Kitob sarlavhasi va muallifi bir xil bo'lgan kitobni yuklay olmaysiz"
+                }
+            )
+
+        return data
+
+    def validate_price(self, price):
+        if price < 0 or price > 999999999:
+            raise  ValidationError(
+                {
+                    "status": False,
+                    "message": "Narx notog'ri kiritilgan"
+                }
+            )
 
 # class BookSerializer(serializers.Serializer):
 #     title = serializers.CharField(max_length=200)
